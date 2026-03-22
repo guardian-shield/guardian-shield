c8ac4396e9a53a6ce9d02ae23300a8bda0fd0287
Maycon Barbosa dos santos <mayconsantos@MacBook-Air-de-Maycon.local>
2026-03-22 18:53:46 -0300

    fix database clean

--- database.py
+++ database.py
@@ -1,9 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://mayconsantos@localhost/guardian"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL não configurado")
 
 engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
